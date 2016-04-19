6aff668883ca75e886beda25943cd63a9c57ca91
bsadams1 <bsadams1@my.waketech.edu>
2016-04-19 10:19:30 -0400

    MVC Controller

--- Spring/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ Spring/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,13 +1,31 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDate;
+
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository mGifRepository;
+
     @RequestMapping("/")
     public String listGifs(){
         return "home";
     }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap){
+        Gif gif = mGifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
+    }
+
 }
---
+++ Spring/src/test/java/com/teamtreehouse/giflib/controller/GifControllerTest.groovy
@@ -0,0 +1,7 @@
+package com.teamtreehouse.giflib.controller
+
+/**
+ * Created by Brandon on 4/17/2016.
+ */
+class GifControllerTest extends GroovyTestCase {
+}
