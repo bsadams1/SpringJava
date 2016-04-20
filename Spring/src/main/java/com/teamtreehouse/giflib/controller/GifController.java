05f409fa449817668165c21097d3ccd0f64465ec
bsadams1 <bsadams1@my.waketech.edu>
2016-04-19 20:17:22 -0400

    GifController

--- Spring/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ Spring/src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -10,20 +10,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Controller
 public class GifController {
     @Autowired
-    private GifRepository mGifRepository;
+    private GifRepository gifRepository;
 
     @RequestMapping("/")
-    public String listGifs(){
+    public String listGifs(ModelMap modelMap)
+    {
+        List<Gif> allGifs = gifRepository.getAllGifs();
+        modelMap.put("gifs", allGifs);
         return "home";
     }
 
     @RequestMapping("/gif/{name}")
     public String gifDetails(@PathVariable String name, ModelMap modelMap){
-        Gif gif = mGifRepository.findByName(name);
+        Gif gif = gifRepository.findByName(name);
         modelMap.put("gif", gif);
         return "gif-details";
     }
